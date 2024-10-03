f67340d01b1fc078c8c78a167067bb5dbf6ced3c
Tanishk Sarathe <123590433+Tanishk786@users.noreply.github.com>
2024-10-03 17:42:27 +0530

    Create BMI-Calculator.js

---
+++ BMI-Calculator.js
@@ -0,0 +1,17 @@
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const height = parseInt(document.querySelector("#height").value);
+      const weight = parseInt(document.querySelector("#weight").value);
+      const results = document.querySelector("#result");
+
+      if (height === "" || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please Enetr the correct value. ${height} is invalid`;
+      } else if (weight === "" || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please Enetr the correct value. ${weight} is invalid`;
+      } else {
+        const formula = (weight / ((height * height) / 10000)).toFixed(2);
+        results.innerHTML = `<span>${formula}</span>`;
+      }
+    });
